a4eeeec31c7890a5db6113f72ee036e8e530967c
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-28 11:53:41 +0100

    adding express server

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
-var ascii = require('ascii-faces');
+// var ascii = require('ascii-faces');
 
-exports.handler = async (event) => {
-  // TODO implement
-  const response = {
-      statusCode: 200,
-      body: JSON.stringify(ascii()),
-  };
-  return response;
-};
+// exports.handler = async (event) => {
+//   // TODO implement
+//   const response = {
+//       statusCode: 200,
+//       body: JSON.stringify(ascii()),
+//   };
+//   return response;
+// };
+
+const express = require('express');
+require('dotenv').config();
+const app = express();
+const serverless = require('serverless-http');
+const port = 8000;
+const { ENVIRONMENT } = process.env;
+
+app.get('/', (req, res) => {
+  res.send({copy: hiya});
+});
+
+app.get('/api', (req, res) => {
+  res.send({copy: 'Hello World!'});
+});
+
+if (ENVIRONMENT !== 'DEV') {
+  module.exports.handler = serverless(app);
+} else {
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+  });
+}
