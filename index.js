eefe9c09eebbe7a1be6206e97592e600d835e8cb
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-28 13:00:19 +0100

    init static to app

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const serverless = require('serverless-http');
 const port = 8000;
 const { ENVIRONMENT } = process.env;
 
+app.use(express.static(path.join(__dirname, 'frontend/build')))
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, './frontend/build', 'index.html'));
 });
