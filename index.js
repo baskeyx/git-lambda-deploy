c323d461f8dd7f031e4ab253f177726663d0bc45
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-28 17:50:39 +0100

    setting up backend structure

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const serverless = require('serverless-http');
 require('dotenv').config();
+
+const characterRoutes = require('./routes/characters');
+
 const app = express();
 app.use(cors());
-const serverless = require('serverless-http');
+
 const port = 8000;
 const { ENVIRONMENT } = process.env;
 
-app.get('/api', (req, res) => {
-  res.send({copy: 'Hello World!'});
-});
+app.use('/api/characters', characterRoutes);
 
 if (ENVIRONMENT !== 'DEV') {
   module.exports.handler = serverless(app);
