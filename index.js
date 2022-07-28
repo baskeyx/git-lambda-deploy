4431d779a9131db7a51afda6a51034620cad18f6
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-28 13:06:34 +0100

    chaning path

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const { ENVIRONMENT } = process.env;
 app.use(express.static(path.join(__dirname, 'frontend/build')))
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, './frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
 });
 
 app.get('/api', (req, res) => {
