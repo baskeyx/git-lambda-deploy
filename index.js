25b8e2a10f3f728cbc4f9bc3c7f1dc04a4d94581
baskeyx <daniel.baskeyfield@gmail.com>
2022-07-28 13:14:25 +0100

    changing path to use resolve

--- index.js
+++ index.js
@@ -8,12 +8,10 @@ const serverless = require('serverless-http');
 const port = 8000;
 const { ENVIRONMENT } = process.env;
 
-//app.use(express.static(path.join(__dirname, 'frontend/build')))
-app.use(express.static('./frontend/build'))
+app.use(express.static(path.resolve(__dirname, 'frontend/build')))
 
 app.get('/', function (req, res) {
-  res.sendFile('./frontend/build/index.html');
-  // res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'frontend/build', 'index.html'));
 });
 
 app.get('/api', (req, res) => {
